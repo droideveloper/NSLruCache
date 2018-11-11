51e9129f2962100a25f9cbff268b6975da250941
droideveloper <fatih.sen.boun@gmail.com>
2018-11-11 11:37:22 +0300

    inital

---
+++ NSLruCache/CacheExpiry.swift
@@ -0,0 +1,15 @@
+//
+//  CacheExpiry.swift
+//  NSLruCache
+//
+//  Created by Fatih Şen on 11.11.2018.
+//  Copyright © 2018 Fatih Şen. All rights reserved.
+//
+
+import Foundation
+
+public enum CacheExpiry {
+	case never
+	case seconds(TimeInterval)
+	case time(Date)
+}
---
+++ NSLruCache/NSLruCache.h
@@ -0,0 +1,19 @@
+//
+//  NSLruCache.h
+//  NSLruCache
+//
+//  Created by Fatih Şen on 11.11.2018.
+//  Copyright © 2018 Fatih Şen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSLruCache.
+FOUNDATION_EXPORT double NSLruCacheVersionNumber;
+
+//! Project version string for NSLruCache.
+FOUNDATION_EXPORT const unsigned char NSLruCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSLruCache/PublicHeader.h>
+
+
